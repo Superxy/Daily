32a629ab84ec7f09fc9f29834d7b1b859853ee3c
Superxy <396415534@qq.com>
2016-04-21 21:06:37 +0800

    difference between private,public and protected inheritance

---
+++ accessLevel.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+/*
+ *	IMPORTANT NOTE: Classes B, C and D all contain the variables x, y and z. It is just question of access.
+ * */
+
+class A 
+{
+public:
+    int x;
+protected:
+    int y;
+private:
+    int z;
+};
+
+class B : public A
+{
+    // x is public
+    // y is protected
+    // z is not accessible from B};
+
+class C : protected A
+{
+    // x is protected
+    // y is protected
+    // z is not accessible from C
+};
+
+class D : private A    // 'private' is default for classes{
+    // x is private
+    // y is private
+    // z is not accessible from D
+};
+
